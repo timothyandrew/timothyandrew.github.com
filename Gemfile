68e8d26cf121f4526b5bcaeb5e505eb82f722c96
Timothy Andrew <mail@timothyandrew.net>
2017-03-20 16:34:03 +0530

    add deploy config

--- Gemfile
+++ Gemfile
@@ -11,3 +11,4 @@ gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
 # Middleman Gems
 gem 'middleman', '~> 4.2'
 gem 'middleman-livereload', '~> 3.4'
+gem 'middleman-deploy', '~> 2.0.0.pre.alpha'
--- config.rb
+++ config.rb
@@ -42,3 +42,8 @@ configure :build do
   # Minify Javascript on build
   # activate :minify_javascript
 end
+
+activate :deploy do |deploy|
+  deploy.deploy_method = :git
+  deploy.branch   = 'master'
+end
